29d28819520c74274f6da0e44d6713d9deb3940f
Sahil Sahu <sahusahil553@gmail.com>
2026-03-08 00:00:55 +0530

    comment

--- 05_TimeLine_animation/Script5.js
+++ 05_TimeLine_animation/Script5.js
@@ -1,1 +1,30 @@
-var tl = gsap.timeline()
+var menu =document.querySelector(".nav i");
+var cross =document.querySelector(".slide i");
+
+var tl = gsap.timeline();
+tl.to(".slide",{
+    right: 0, 
+    duration: 0.6,
+    stagger: 0.27
+});
+
+tl.from(".slide h4",{
+    opacity: 0,
+x: 150,
+duration: 0.37,
+stagger: 0.27
+})
+
+tl.from(".slide i",{
+    opacity: 0,
+    duration: 0.37,
+    stagger: 0.27
+})
+tl.pause();
+
+menu.addEventListener("click",()=>{
+    tl.play();
+});
+cross.addEventListener("click",()=>{
+    tl.reverse();
+});
